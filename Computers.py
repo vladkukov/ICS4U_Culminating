1a72bb776f17eb20b9f82cedcc61a027113f6e7c
jrond <vlad@kukov.com>
2022-06-08 11:41:23 -0400

    hh

---
+++ Computers.py
@@ -0,0 +1,12 @@
+def __init__(self, modelNum, manufacturer, ramSize, storage, processorSpeed, price):
+    self.modelNum = modelNum
+    self.manufacturer = manufacturer
+    self.ramSize = ramSize
+    self.storage = storage
+    self.processorSpeed = processorSpeed
+    self.price = price
+
+"""
+def getModelNum(self):
+    return self.modelNum
+"""
